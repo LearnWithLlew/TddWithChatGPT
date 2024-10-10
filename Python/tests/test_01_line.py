a73a679de1ed6c80b681665453047a4e138b9982
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-10-10 11:30:22 -0600

    Update test_01_line.py

--- Python/tests/test_01_line.py
+++ Python/tests/test_01_line.py
@@ -2,6 +2,7 @@
 
 def test_simple_replace():
     # Create a line of "one " that has a "too " removed and a "two " added and ends with "three."
+    # Check that there are 4 parts in the line
     # Check that first parts of the line is unchanged
     # Check that second parts of the line is removed
     # Check that third parts of the line is added
